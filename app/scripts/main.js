9b28143011dc858ebcb1c1148a7b3fd17a69ebc1
Roman Gotsiy <gotsijroman@gmail.com>
2014-12-24 00:37:13 +0200

    Fixed missing slick.js fonts + minor fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,10 @@
-'use strict';
-
-$(document).ready(function(){
-	$('.single-item').slick({
-    dots: true,
-		autoplay: true,
-  	autoplaySpeed: 4000,
-  });
-});
+(function () {
+   'use strict';
+   $(document).ready(function(){
+			$('.single-item').slick({
+				dots: true,
+				autoplay: true,
+				autoplaySpeed: 4000,
+			});
+		});
+}());
