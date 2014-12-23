f05ee6f663f3aaba8da03bf81cd92a7c6ceca62f
Roman Gotsiy <gotsijroman@gmail.com>
2014-12-24 00:08:12 +0200

    Added carousel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,9 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$(document).ready(function(){
+	$('.single-item').slick({
+    dots: true,
+		autoplay: true,
+  	autoplaySpeed: 4000,
+  });
+});
